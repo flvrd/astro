d452f1bb0890986911f750c4866c14f5656b9028
flvr <flvr.pill@gmail.com>
2024-06-26 14:30:59 -0500

    final?

---
+++ electrical-eclipse/astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import netlify from "@astrojs/netlify";
+
+import { defineConfig } from "astro/config";
+import netlify from "@astrojs/netlify";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind()],
+  output: "server",
+  adapter: netlify(),
+});
